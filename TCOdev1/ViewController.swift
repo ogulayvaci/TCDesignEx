63a2d25b74973cda98222eb537d50b00bb331606
Ogul Ayvaci <h.ogulayvaci@gmail.com>
2025-04-14 21:54:08 +0300

    Initial Commit

---
+++ TCOdev1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TCOdev1
+//
+//  Created by Ogul Ayvaci on 14.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
